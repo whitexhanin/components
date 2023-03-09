f83aef41537c2b886362d7d8da7c2d44d3d4ba25
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2023-03-09 16:33:15 +0900

    추가

--- Overview/index.js
+++ Overview/index.js
@@ -3,6 +3,12 @@ window.addEventListener('load',function(){
     itemList.forEach(element => {
         element.addEventListener('click',itemClick);    
     });
+
+    let moreList = document.querySelectorAll('.itemlist .more');
+    moreList.forEach(element => {
+        element.addEventListener('click',moreClick);    
+    });
+
     
 })
 
@@ -10,10 +16,45 @@ const itemClick = (e) => {
     let target = e.currentTarget;
     let name = target.innerText;
     let iframe = document.querySelector('iframe');
+    let active = document.querySelector('.itemlist button.active');    
+    let more = document.querySelector('.more');
+    let subList = target.closest('.sublist');
+    let targetLi;
+    let targetMore;
+    let moreTxt;    
 
-    iframe.setAttribute('src','../ '+ name + '/index.html');
     
-    
+    if(!target.classList.contains('more')){
+        active.classList.remove('active');
+        target.classList.add('active');        
 
-    setAtt
-}
+        //타겟이 서브 메뉴 일때 or 아닐때
+        if(subList) {
+            targetLi = subList.closest('li');
+            targetMore = targetLi.querySelector('.more');            
+            moreTxt = targetMore.innerText;  
+            iframe.setAttribute('src','../'+ moreTxt+'/'+ name +'/index.html');  
+        }else{
+            iframe.setAttribute('src','../'+ name +'/index.html');  
+            if(more.classList.contains('open')){
+                more.classList.remove('open');
+            }
+        }
+
+        
+    }    
+    
+ }
+ const moreClick = (e) => {    
+    let target = e.currentTarget;    
+    let subList = target.closest('li').querySelector('.sublist');
+    let subLength = subList.childElementCount;
+    
+    if(target.classList.contains('open')){
+        target.classList.remove('open');
+        subList.style.height = '0';
+    }else {
+        target.classList.add('open');
+        subList.style.height = 'calc(48px *'+ subLength+')';
+    }
+ }
